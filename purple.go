8c9ac86ee9a89562f07d5cac8ff4ddc95dadb664
superwhiskers <kspy360@rocketmail.com>
2018-09-06 21:58:09 +0000

    tfw you forgot that godoc can't add newlines in package comments

--- purple.go
+++ purple.go
@@ -1,6 +1,6 @@
 // Package purple is a library that attempts to provide utilities for other libraries and programs to use
 //
-// purple is subject to the terms of the gnu lesser general public license version 3
-// you should've been distributed a copy of the lesser general public license and the general public license version 3 along with this software.
-// if you do not have either and would like them you can get them at https://www.gnu.org/licenses/gpl.txt (please read through them)
+// purple is subject to the terms of the gnu lesser general public license version 3.
+// you should've been distributed a copy of the lesser general public license and the general public license version 3 along with this library.
+// if you do not have either and would like them, you can get them at https://www.gnu.org/licenses/gpl.txt (please read through them)
 package purple
